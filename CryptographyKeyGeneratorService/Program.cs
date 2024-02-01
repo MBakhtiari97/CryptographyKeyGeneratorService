096d85f8b275607dd666033fedc53fd99213ad22
MBakhtiari97 <mohammadba86@yahoo.com>
2024-02-01 20:04:02 +0330

    Final program class

--- CryptographyKeyGeneratorService/Program.cs
+++ CryptographyKeyGeneratorService/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using CryptographyKeyGeneratorService;
+using System.Security.Cryptography;
+using (var rsaCryptoService = new RSACryptoServiceProvider(2048))
+{
+    // Export the private key
+    string privateKey = PrivateKeyGenerator.Generate(rsaCryptoService);
+
+    // Print the private key to the console
+    Console.WriteLine("Test Private Key:");
+    Console.WriteLine(privateKey);
+
+    // Optionally, you can also export the public key
+    string publicKey = PublicKeyGenerator.Generate(rsaCryptoService);
+    Console.WriteLine("\nTest Public Key:");
+    Console.WriteLine(publicKey);
+}
+
+Console.ReadLine();
