6fb97b4272b3bc473ee4ffd748050b78c57fe731
MBakhtiari97 <mohammadba86@yahoo.com>
2024-02-01 20:03:16 +0330

    PrivateKeyGenerator service

---
+++ CryptographyKeyGeneratorService/PrivateKeyGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+namespace CryptographyKeyGeneratorService;
+internal static class PrivateKeyGenerator
+{
+    public static string Generate(RSACryptoServiceProvider rsaCryptoService)
+    {
+        // Export the private key
+        string privateKey = rsaCryptoService.ToXmlString(true);
+        return privateKey;
+    }
+}
